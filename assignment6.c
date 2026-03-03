92541522587a44cade0dfd8b9ab268c4d83365d0
Vedant0026 <1262251623@mitwpu.edu.in>
2026-03-03 23:15:48 +0530

    assignment6.c

---
+++ assignment6.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+struct Student {
+    int rollNo;
+    char name[50];
+    float marks[5];
+    float total;
+    float percentage;
+    char grade;
+};
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    struct Student s[n];
+
+    for(i = 0; i < n; i++) {
+        printf("\nEnter details for Student %d\n", i + 1);
+
+        printf("Roll Number: ");
+        scanf("%d", &s[i].rollNo);
+
+        printf("Name: ");
+        scanf("%s", s[i].name);
+
+        s[i].total = 0;
+
+        printf("Enter marks of 5 subjects:\n");
+        for(j = 0; j < 5; j++) {
+            scanf("%f", &s[i].marks[j]);
+            s[i].total += s[i].marks[j];
+        }
+
+        s[i].percentage = s[i].total / 5;
+
+        // Grade calculation
+        if(s[i].percentage >= 90)
+            s[i].grade = 'A';
+        else if(s[i].percentage >= 75)
+            s[i].grade = 'B';
+        else if(s[i].percentage >= 60)
+            s[i].grade = 'C';
+        else if(s[i].percentage >= 40)
+            s[i].grade = 'D';
+        else
+            s[i].grade = 'F';
+    }
+
+    printf("\n----- Student Results -----\n");
+
+    for(i = 0; i < n; i++) {
+        printf("\nStudent %d\n", i + 1);
+        printf("Roll No: %d\n", s[i].rollNo);
+        printf("Name: %s\n", s[i].name);
+        printf("Total Marks: %.2f\n", s[i].total);
+        printf("Percentage: %.2f%%\n", s[i].percentage);
+        printf("Grade: %c\n", s[i].grade);
+    }
+
+    return 0;
+}
